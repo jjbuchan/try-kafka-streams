5c9a9da74a0a7d988636a62da9c531f9dde2a049
James Buchan <james.buchan@rackspace.com>
2019-02-04 14:57:13 -0700

    Commit all files

---
+++ src/main/java/me/jjbuchan/trykafkastreams/TryKafkaStreamsApplication.java
@@ -0,0 +1,14 @@
+package me.jjbuchan.trykafkastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryKafkaStreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryKafkaStreamsApplication.class, args);
+    }
+
+}
+
